ee2a910ac809a1404df03066344c575799929aca
Crispin Wellington <retrogradeorbit@gmail.com>
2016-01-11 22:46:39 +0800

    remove async dep

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.2.374"]
                  [com.domkm/whois "0.0.1"]]
   :main ^:skip-aot geoip.core
   :target-path "target/%s"
